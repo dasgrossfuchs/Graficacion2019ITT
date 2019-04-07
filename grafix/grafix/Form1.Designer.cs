1afea79b77ae1c41d30645370e0525a7d94d11cf
dasgrossfuchs <agustinabta@gmail.com>
2019-04-06 18:46:00 -0700

    Update Form1.Designer.cs

--- grafix/grafix/Form1.Designer.cs
+++ grafix/grafix/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(184, 229);
+            this.pictureBox1.Location = new System.Drawing.Point(352, 241);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(50, 55);
             this.pictureBox1.TabIndex = 0;
